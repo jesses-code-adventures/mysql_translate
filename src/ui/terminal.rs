f2c6b30485eb51585359b880ef592ab1ab0aceb0
Jesse Williams <jesse@negativespacesounds.com>
2023-12-08 18:56:46 +1100

    update: add crossterm to terminal implementation

--- src/ui/terminal.rs
+++ src/ui/terminal.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal::{Clear, ClearType},
+};
 use dialoguer::Select;
 use std::cell::RefCell;
+use std::process::exit;
 
 use crate::{
     functionality::{database, session::Session, structure::AcceptedFormat},
@@ -22,17 +28,41 @@ static GOODBYE_STRING: &str = "goodbye!";
 static NEWLINES_STRING: &str = "\n\n";
 
 /// A TUI implementation for mysql translate.
-/// Probably not the nicest way to do this.
 pub struct TerminalUI {
     pub session: RefCell<Session>,
 }
 
 impl TerminalUI {
-    fn welcome(&self) {
-        println!("{}", WELCOME_STRING);
+    fn happy_message(&self, message: String) {
+        execute!(
+            std::io::stdout(),
+            SetForegroundColor(Color::DarkGreen),
+            Print(message),
+            ResetColor
+        )
+        .expect("print to work");
     }
 
-    fn clear_terminal(&self) {
+    fn selected_message(&self, message: String) {
+        execute!(
+            std::io::stdout(),
+            SetForegroundColor(Color::Magenta),
+            Print(message),
+            ResetColor
+        )
+        .expect("print to work");
+    }
+
+    fn welcome(&self) {
+        self.happy_message(WELCOME_STRING.to_string());
+    }
+
+    fn clear_terminal_line(&self) {
+        // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        Clear(ClearType::CurrentLine);
+    }
+
+    fn clear_whole_terminal(&self) {
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     }
 
@@ -44,12 +74,13 @@ impl TerminalUI {
         println!("{}", NEWLINES_STRING);
     }
 
+    /// General validator to ensure input isn't empty and hasn't failed
     fn prompt_user_until_successful(&self, prompt: String) -> String {
         let mut is_empty = false;
         let mut input_failed = false;
         let mut input = String::new();
         loop {
-            self.clear_terminal();
+            self.clear_terminal_line();
             if is_empty {
                 println!("input cannot be empty");
             }
@@ -81,7 +112,7 @@ impl UI for TerminalUI {
     /// Create a new TerminalUI
     fn new(session: RefCell<Session>) -> Self {
         let resp = TerminalUI { session };
-        resp.clear_terminal();
+        resp.clear_whole_terminal();
         resp.welcome();
         resp
     }
@@ -109,7 +140,11 @@ impl UI for TerminalUI {
                     .map(|database| database.name.as_str())
                     .collect::<Vec<_>>(),
             )
-            .interact()?;
+            .interact_opt()?
+            .unwrap_or_else(|| {
+                self.goodbye();
+                exit(0)
+            });
         Ok(selection)
     }
 
@@ -140,14 +175,18 @@ impl UI for TerminalUI {
     }
 
     fn select_schema_to_write(&mut self) -> Result<()> {
-        self.clear_terminal();
+        self.clear_terminal_line();
         let db_index = self.select_database()?;
         let options = AcceptedFormat::all_as_string_array();
         let selection = Select::new()
             .with_prompt("which schema would you like to write to the disk?")
             .default(0)
             .items(&options)
-            .interact()?;
+            .interact_opt()?
+            .unwrap_or_else(|| {
+                self.goodbye();
+                exit(0)
+            });
         match self
             .get_session()
             .write_one_schema_from_database(selection, db_index, &options)
@@ -164,14 +203,18 @@ impl UI for TerminalUI {
     }
 
     fn view_tables_from_disk(&self) -> Result<()> {
-        self.clear_terminal();
+        self.clear_terminal_line();
         let db_index = self.select_database()?;
         let options = AcceptedFormat::all_as_string_array();
         let selection = Select::new()
             .with_prompt("which schema would you like to view from the disk?")
             .default(0)
             .items(&options)
-            .interact()?;
+            .interact_opt()?
+            .unwrap_or_else(|| {
+                self.goodbye();
+                exit(0)
+            });
         println!(
             "{}",
             self.get_session()
@@ -181,14 +224,18 @@ impl UI for TerminalUI {
     }
 
     fn view_tables_from_database(&self) -> Result<()> {
-        self.clear_terminal();
+        self.clear_terminal_line();
         let db_index = self.select_database()?;
         let options = AcceptedFormat::all_as_string_array();
         let selection = Select::new()
             .with_prompt("which schema would you like view from the database?")
             .default(0)
             .items(&options)
-            .interact()?;
+            .interact_opt()?
+            .unwrap_or_else(|| {
+                self.goodbye();
+                exit(0)
+            });
         println!(
             "{}",
             self.get_session()
@@ -214,14 +261,18 @@ impl UI for TerminalUI {
                 .with_prompt("what would you like to do?")
                 .default(0)
                 .items(&MAIN_MENU_ITEMS)
-                .interact()?;
+                .interact_opt()?
+                .unwrap_or_else(|| {
+                    self.goodbye();
+                    exit(0)
+                });
             match selection {
                 0 => {
-                    self.clear_terminal();
+                    self.clear_terminal_line();
                     self.get_session_mut().display();
                 }
                 1 => {
-                    self.clear_terminal();
+                    self.clear_terminal_line();
                     self.get_session_mut().sync()
                 }
                 2 => self.select_schema_to_write()?,
